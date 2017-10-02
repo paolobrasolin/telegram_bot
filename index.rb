7aa2be9988c8a9fb5cfc013d3a36e413fbf34f83
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-10-02 21:32:27 +0200

    test

--- index.rb
+++ index.rb
@@ -1,39 +1,47 @@
-# require "openssl"
-# require 'net/http'
+require "openssl"
+require 'net/http'
+
+require 'json'
+
+puts Hook.datastore
 
 
 chat_id = Hook['params']['message']['chat']['id']
 user = Hook['params']['message']['from']['first_name']
 text = Hook['params']['message']['text']
 
-# # DO STUFF HERE
+# DO STUFF HERE
 
-# require 'json'
-# text = Hook['params'].to_json
+text = Hook['params'].to_json
 
-require 'telegramAPI'
+
+
+
+# require 'telegramAPI'
+# token = Hook['env']['autonomous_war_domination_robot_token']
+# api = TelegramAPI.new token
+# api.sendMessage(chat_id, text)
+
+
+
+
+
 
 token = Hook['env']['autonomous_war_domination_robot_token']
+url = 'https://api.telegram.org/bot' + token + '/sendMessage'
+uri = URI url
+payload = { chat_id: chat_id, text: text }
 
-api = TelegramAPI.new token
+http = Net::HTTP.start uri.host, uri.port,
+                       use_ssl: uri.scheme == 'https',
+                       verify_mode: OpenSSL::SSL::VERIFY_NONE
 
-api.sendMessage(chat_id, text)
+uri.query = URI.encode_www_form payload
+request = Net::HTTP::Get.new uri.path + '?' + uri.query
+response = http.request request
 
-# token = Hook['env']['autonomous_war_domination_robot_token']
-# url = 'https://api.telegram.org/bot' + token + '/sendMessage'
-# uri = URI url
-# payload = { chat_id: chat_id, text: text }
+# puts response
 
-# http = Net::HTTP.start uri.host, uri.port,
-#                        use_ssl: uri.scheme == 'https',
-#                        verify_mode: OpenSSL::SSL::VERIFY_NONE
-
-# uri.query = URI.encode_www_form payload
-# request = Net::HTTP::Get.new uri.path + '?' + uri.query
-# response = http.request request
-
-# # puts response
-
-# # puts 'Hello, this is a Ruby script.'
-# # puts "Hook['params'] is populated with request parameters"
-# # puts Hook['params']
+# puts 'Hello, this is a Ruby script.'
+# puts "Hook['params'] is populated with request parameters"
+# puts Hook['params']
