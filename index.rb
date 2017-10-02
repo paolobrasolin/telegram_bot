59889626908c6c4af7a4ab6d43dcc21ac846696a
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-10-02 19:09:03 +0200

    test

--- index.rb
+++ index.rb
@@ -24,9 +24,8 @@ uri.query = URI.encode_www_form payload
 
 Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
   request = Net::HTTP::Get.new uri
-  response = http.request request # Net::HTTPResponse object
+  response = http.request request
   puts response.body #if res.is_a?(Net::HTTPSuccess)
 end
 
