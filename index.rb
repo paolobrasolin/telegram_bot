b77a4fc20037563f20e33f72b27ddd697d0d2f93
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-10-02 19:27:51 +0200

    test

--- index.rb
+++ index.rb
@@ -22,13 +22,14 @@ payload = { chat_id: chat_id, text: text }
 uri = URI url
 uri.query = URI.encode_www_form payload
 
-# Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
-#   http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+  # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-#   request = Net::HTTP::Get.new uri
-#   response = http.request request # Net::HTTPResponse object
-#   puts response.body #if res.is_a?(Net::HTTPSuccess)
-# end
+  request = Net::HTTP::Get.new uri
+  response = http.request request # Net::HTTPResponse object
+  puts response.body #if res.is_a?(Net::HTTPSuccess)
+end
 
 
 
