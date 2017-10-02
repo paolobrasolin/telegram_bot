e7f8c8070a54bd38a7cc473fdef24d2836198ca4
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-10-02 19:50:48 +0200

    test

--- index.rb
+++ index.rb
@@ -1,12 +1,23 @@
 require "openssl"
 require 'net/http'
 
-
-puts "YO!" and return unless Hook['params'].key? 'message'
+require 'json'
 
 chat_id = Hook['params']['message']['chat']['id']
 user = Hook['params']['message']['from']['first_name']
 text = Hook['params']['message']['text']
+
+# DO STUFF HERE
+
+text = Hook['params'].to_json
+
+
+
+
+
+
+
+
 
 token = Hook['env']['autonomous_war_domination_robot_token']
 url = 'https://api.telegram.org/bot' + token + '/sendMessage'
