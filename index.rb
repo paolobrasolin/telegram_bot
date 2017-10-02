6edb34ccd112c769b307ca00a99dd034a89e626c
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-10-02 18:35:50 +0200

    test

--- index.rb
+++ index.rb
@@ -1,11 +1,21 @@
-# import requests
+require 'net/http'
 
-# user=Hook['params']['message']['from']['first_name']
-# testo=u'\U0001F916'+Hook['params']['message']['text'].replace(" ", "").upper()+user.upper()
-# chatID=Hook['params']['message']['chat']['id']
-# URL='https://api.telegram.org/bot' + Hook['env']['autonomous_war_domination_robot_token'] + '/sendMessage'
-# richiesta=requests.get(URL,verify=False,data={'chat_id':chatID,'text':testo})
+chat_id = Hook['params']['message']['chat']['id']
+user = Hook['params']['message']['from']['first_name']
+text = Hook['params']['message']['text']
+token = Hook['env']['autonomous_war_domination_robot_token']
 
-puts 'Hello, this is a Ruby script.'
-puts "Hook['params'] is populated with request parameters"
-puts Hook['params']
+url = 'https://api.telegram.org/bot' + token + '/sendMessage'
+
+payload = { chat_id: chat_id, text: text }
+
+uri = URI url
+
+uri.query = URI.encode_www_form payload
+
+res = Net::HTTP.get_response(uri)
+
+puts res.body #if res.is_a?(Net::HTTPSuccess)
+
+
+puts "Online!"
