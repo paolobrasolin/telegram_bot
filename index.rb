cbe19dfeb91f37872d911dc3f4ae648bd1b726eb
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-10-02 20:55:18 +0200

    test

--- index.rb
+++ index.rb
@@ -3,17 +3,16 @@ require 'net/http'
 
 require 'json'
 
-chat_id = Hook['params']['message']['chat']['id']
-user = Hook['params']['message']['from']['first_name']
-text = Hook['params']['message']['text']
-
-# DO STUFF HERE
-
-text = Hook['params'].to_json
-
 puts Hook.datastore
 
 
+# chat_id = Hook['params']['message']['chat']['id']
+# user = Hook['params']['message']['from']['first_name']
+# text = Hook['params']['message']['text']
+
+# # DO STUFF HERE
+
+# text = Hook['params'].to_json
 
 
 
@@ -21,21 +20,24 @@ puts Hook.datastore
 
 
 
-token = Hook['env']['autonomous_war_domination_robot_token']
-url = 'https://api.telegram.org/bot' + token + '/sendMessage'
-uri = URI url
-payload = { chat_id: chat_id, text: text }
 
-http = Net::HTTP.start uri.host, uri.port,
-                       use_ssl: uri.scheme == 'https',
-                       verify_mode: OpenSSL::SSL::VERIFY_NONE
 
-uri.query = URI.encode_www_form payload
-request = Net::HTTP::Get.new uri.path + '?' + uri.query
-response = http.request request
 
-# puts response
+# token = Hook['env']['autonomous_war_domination_robot_token']
+# url = 'https://api.telegram.org/bot' + token + '/sendMessage'
+# uri = URI url
+# payload = { chat_id: chat_id, text: text }
 
-# puts 'Hello, this is a Ruby script.'
-# puts "Hook['params'] is populated with request parameters"
-# puts Hook['params']
+# http = Net::HTTP.start uri.host, uri.port,
+#                        use_ssl: uri.scheme == 'https',
+#                        verify_mode: OpenSSL::SSL::VERIFY_NONE
+
+# uri.query = URI.encode_www_form payload
+# request = Net::HTTP::Get.new uri.path + '?' + uri.query
+# response = http.request request
+
+# # puts response
+
+# # puts 'Hello, this is a Ruby script.'
+# # puts "Hook['params'] is populated with request parameters"
+# # puts Hook['params']
