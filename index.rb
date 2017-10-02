b8baab6411f295fc5fc2df88c8a496b59c04ebde
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-10-02 18:41:45 +0200

    test

--- index.rb
+++ index.rb
@@ -1,9 +1,10 @@
 require 'net/http'
 
-chat_id = Hook.dig 'params', 'message', 'chat', 'id'
+token = Hook['env']['autonomous_war_domination_robot_token']
+
+# chat_id = Hook['params']['message']['chat']['id']
 # user = Hook['params']['message']['from']['first_name']
 # text = Hook['params']['message']['text']
-# token = Hook['env']['autonomous_war_domination_robot_token']
 
 # url = 'https://api.telegram.org/bot' + token + '/sendMessage'
 
@@ -17,7 +18,7 @@ chat_id = Hook.dig 'params', 'message', 'chat', 'id'
 
 # puts res.body #if res.is_a?(Net::HTTPSuccess)
 
-puts chat_id
+puts Hook['env'].class
 
 puts 'Hello, this is a Ruby script.'
 puts "Hook['params'] is populated with request parameters"
