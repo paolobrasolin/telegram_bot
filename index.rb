1ad48c8accb9aa760e02ef73cd335e58acdf98f6
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-10-02 19:24:21 +0200

    test

--- index.rb
+++ index.rb
@@ -1,7 +1,7 @@
-# require "openssl"
-# puts OpenSSL::OPENSSL_VERSION
-# puts "SSL_CERT_FILE: %s" % OpenSSL::X509::DEFAULT_CERT_FILE
-# puts "SSL_CERT_DIR: %s" % OpenSSL::X509::DEFAULT_CERT_DIR
+require "openssl"
+puts OpenSSL::OPENSSL_VERSION
+puts "SSL_CERT_FILE: %s" % OpenSSL::X509::DEFAULT_CERT_FILE
+puts "SSL_CERT_DIR: %s" % OpenSSL::X509::DEFAULT_CERT_DIR
 
 require 'net/http'
 
