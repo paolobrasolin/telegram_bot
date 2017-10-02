90a6f6113891a9d95528157db45d86e90dd9139b
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-10-02 21:41:21 +0200

    test

--- index.rb
+++ index.rb
@@ -12,35 +12,26 @@ text = 'YO' # Hook['params']['message']['text']
 
 # text = Hook['params'].to_json
 
-begin
-  require 'progressbar'
-rescue LoadError => e
-  puts "exception .. installing with gem"
-  puts `gem install progressbar`
-  Gem.clear_paths
-  require 'progressbar'
-end
-
-# require 'telegramAPI'
+require 'telegramAPI'
 # token = Hook['env']['autonomous_war_domination_robot_token']
 # api = TelegramAPI.new token
 # api.sendMessage(chat_id, text)
 
-token = Hook['env']['autonomous_war_domination_robot_token']
-url = 'https://api.telegram.org/bot' + token + '/sendMessage'
-uri = URI url
-payload = { chat_id: chat_id, text: text }
+# token = Hook['env']['autonomous_war_domination_robot_token']
+# url = 'https://api.telegram.org/bot' + token + '/sendMessage'
+# uri = URI url
+# payload = { chat_id: chat_id, text: text }
 
-http = Net::HTTP.start uri.host, uri.port,
-                       use_ssl: uri.scheme == 'https',
-                       verify_mode: OpenSSL::SSL::VERIFY_NONE
+# http = Net::HTTP.start uri.host, uri.port,
+#                        use_ssl: uri.scheme == 'https',
+#                        verify_mode: OpenSSL::SSL::VERIFY_NONE
 
-uri.query = URI.encode_www_form payload
-request = Net::HTTP::Get.new uri.path + '?' + uri.query
-response = http.request request
+# uri.query = URI.encode_www_form payload
+# request = Net::HTTP::Get.new uri.path + '?' + uri.query
+# response = http.request request
 
-# puts response
+# # puts response
 
-# puts 'Hello, this is a Ruby script.'
-# puts "Hook['params'] is populated with request parameters"
-# puts Hook['params']
+# # puts 'Hello, this is a Ruby script.'
+# # puts "Hook['params'] is populated with request parameters"
+# # puts Hook['params']
